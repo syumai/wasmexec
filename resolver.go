2b4c10b91b7d573e587ee617dc9564f034d8d781
syumai <syumai@gmail.com>
2020-02-12 00:54:44 +0900

    Split create function signature func

--- resolver.go
+++ resolver.go
@@ -25,6 +25,85 @@ func toValueTypeFromKind(k reflect.Kind) (wasm.ValueType, error) {
 	return 0, fmt.Errorf("unsupported kind: %q", k.String())
 }
 
+func createFunctionSignature(name string, fn interface{}, index uint32) (wasm.FunctionSig, wasm.Function, wasm.ExportEntry) {
+	t := reflect.TypeOf(fn)
+
+	// Create FunctionSignature
+	sig := wasm.FunctionSig{Form: 0}
+
+	var ins, outs []reflect.Type
+	ins = append(ins, reflect.TypeOf(&exec.Process{}))
+
+	for i := 0; i < t.NumIn(); i++ {
+		p := t.In(i)
+		if p.Kind() == reflect.Int {
+			// Always treat int as int32
+			p = reflect.TypeOf(int32(0))
+		}
+		ins = append(ins, p)
+		vt, err := toValueTypeFromKind(p.Kind())
+		if err != nil {
+			fmt.Errorf("failed to convert kind of param %q: %w", p.Name(), err)
+		}
+		sig.ParamTypes = append(sig.ParamTypes, vt)
+	}
+	for i := 0; i < t.NumOut(); i++ {
+		p := t.Out(i)
+		if p.Kind() == reflect.Int {
+			// Always treat int as int32
+			p = reflect.TypeOf(int32(0))
+		}
+		outs = append(outs, p)
+		vt, err := toValueTypeFromKind(p.Kind())
+		if err != nil {
+			fmt.Errorf("failed to convert kind of return value %q: %w", p.Name(), err)
+		}
+		sig.ReturnTypes = append(sig.ReturnTypes, vt)
+	}
+
+	fnType := reflect.FuncOf(ins, outs, t.IsVariadic())
+	fnValue := reflect.MakeFunc(fnType, func(args []reflect.Value) []reflect.Value {
+		var realArgs []reflect.Value
+		for i := 1; i < len(args); i++ { // ignore *exec.Process
+			p := t.In(i - 1)
+			if p.Kind() == reflect.Int {
+				realArgs = append(realArgs, reflect.ValueOf(args[i].Interface()).Convert(reflect.TypeOf(0)))
+				continue
+			}
+			realArgs = append(realArgs, args[i])
+		}
+		origFunc := reflect.ValueOf(fn)
+		returnValues := origFunc.Call(realArgs)
+
+		var realReturnValues []reflect.Value
+		for i := 0; i < len(returnValues); i++ {
+			p := t.Out(i)
+			if p.Kind() == reflect.Int {
+				realReturnValues = append(realReturnValues, reflect.ValueOf(returnValues[i].Interface()).Convert(reflect.TypeOf(int32(0))))
+				continue
+			}
+			realReturnValues = append(realReturnValues, returnValues[i])
+		}
+		return realReturnValues
+	})
+
+	// Create FunctionIndexSpace
+	indexSpace := wasm.Function{
+		Sig:  &sig,
+		Host: fnValue,
+		Body: &wasm.FunctionBody{}, // dummy wasm body
+	}
+
+	// Add Function to Export
+	exportEntry := wasm.ExportEntry{
+		FieldStr: name,
+		Kind:     wasm.ExternalFunction,
+		Index:    index,
+	}
+
+	return sig, indexSpace, exportEntry
+}
+
 func generateResolverFromImportObject(importObject ImportObject) wasm.ResolveFunc {
 	return func(name string) (*wasm.Module, error) {
 		obj, ok := importObject[name]
@@ -36,83 +115,14 @@ func generateResolverFromImportObject(importObject ImportObject) wasm.ResolveFun
 		var index uint32
 		for k, v := range obj {
 			t := reflect.TypeOf(v)
-			if t.Kind() != reflect.Func {
+			switch t.Kind() {
+			case reflect.Func:
+				sig, indexSpace, exportEntry := createFunctionSignature(k, v, index)
+				m.Types.Entries = append(m.Types.Entries, sig)
+				m.FunctionIndexSpace = append(m.FunctionIndexSpace, indexSpace)
+				m.Export.Entries[k] = exportEntry
+			default:
 				return nil, fmt.Errorf("kind %q is not supported for multiply object", t.Kind())
-			}
-
-			// Create FunctionSignature
-			sig := wasm.FunctionSig{Form: 0}
-
-			var ins, outs []reflect.Type
-			ins = append(ins, reflect.TypeOf(&exec.Process{}))
-
-			for i := 0; i < t.NumIn(); i++ {
-				p := t.In(i)
-				if p.Kind() == reflect.Int {
-					// Always treat int as int32
-					p = reflect.TypeOf(int32(0))
-				}
-				ins = append(ins, p)
-				vt, err := toValueTypeFromKind(p.Kind())
-				if err != nil {
-					fmt.Errorf("failed to convert kind of param %q: %w", p.Name(), err)
-				}
-				sig.ParamTypes = append(sig.ParamTypes, vt)
-			}
-			for i := 0; i < t.NumOut(); i++ {
-				p := t.Out(i)
-				if p.Kind() == reflect.Int {
-					// Always treat int as int32
-					p = reflect.TypeOf(int32(0))
-				}
-				outs = append(outs, p)
-				vt, err := toValueTypeFromKind(p.Kind())
-				if err != nil {
-					fmt.Errorf("failed to convert kind of return value %q: %w", p.Name(), err)
-				}
-				sig.ReturnTypes = append(sig.ReturnTypes, vt)
-			}
-			m.Types.Entries = append(m.Types.Entries, sig)
-
-			fnType := reflect.FuncOf(ins, outs, t.IsVariadic())
-			fnValue := reflect.MakeFunc(fnType, func(args []reflect.Value) []reflect.Value {
-				var realArgs []reflect.Value
-				for i := 1; i < len(args); i++ { // ignore *exec.Process
-					p := t.In(i - 1)
-					if p.Kind() == reflect.Int {
-						realArgs = append(realArgs, reflect.ValueOf(args[i].Interface()).Convert(reflect.TypeOf(0)))
-						continue
-					}
-					realArgs = append(realArgs, args[i])
-				}
-				origFunc := reflect.ValueOf(v)
-				returnValues := origFunc.Call(realArgs)
-
-				var realReturnValues []reflect.Value
-				for i := 0; i < len(returnValues); i++ {
-					p := t.Out(i)
-					if p.Kind() == reflect.Int {
-						realReturnValues = append(realReturnValues, reflect.ValueOf(returnValues[i].Interface()).Convert(reflect.TypeOf(int32(0))))
-						continue
-					}
-					realReturnValues = append(realReturnValues, returnValues[i])
-				}
-				return realReturnValues
-			})
-
-			// Create FunctionIndexSpace
-			indexSpace := wasm.Function{
-				Sig:  &sig,
-				Host: fnValue,
-				Body: &wasm.FunctionBody{}, // dummy wasm body
-			}
-			m.FunctionIndexSpace = append(m.FunctionIndexSpace, indexSpace)
-
-			// Add Function to Export
-			m.Export.Entries[k] = wasm.ExportEntry{
-				FieldStr: k,
-				Kind:     wasm.ExternalFunction,
-				Index:    index,
 			}
 			index++
 		}
